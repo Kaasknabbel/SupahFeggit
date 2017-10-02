ffc151a114acedbe25d6c0450c6fff629caa2cda
Kaasknabbel <32313522+Kaasknabbel@users.noreply.github.com>
2017-10-02 22:42:19 +0200

    Update trackhelper.js

--- components/trackhelper.js
+++ components/trackhelper.js
@@ -30,11 +30,10 @@ TrackHelper.prototype.getRandomTrack = function(searchWord, amount) {
   var baseUrl = 'https://www.youtube.com/watch?v=';
 
   return new Promise(function(resolve, reject) {
-    youTube.search(searchWord, amount, function(error, result) {
+    youTube.search(searchWord, amount, function(error, results) {
       if (error) reject('No videos found.');
 
-      //result.items.forEach(function(item) {
-      for (var item : result.items) {
+      results.items.forEach(function(item) {
         if (item.id.videoId) {
           var url = 'https://www.youtube.com/watch?v=' + item.id.videoId;
           trackList.push(new Track(buildTrack(item, url)));
