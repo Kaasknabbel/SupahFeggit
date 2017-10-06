13e310e5e268755b607695f4624b65fb97c66dcf
Kaasknabbel <32313522+Kaasknabbel@users.noreply.github.com>
2017-10-06 11:19:07 +0200

    Update dumpert.js

--- components/dumpert.js
+++ components/dumpert.js
@@ -9,7 +9,8 @@ module.exports = Dumpert = function() {
 
 Dumpert.prototype.getTop5 = function(args, message) {
   request('http://www.dumpert.nl/toppers/', (error, response, body) => {
+    var bodystr = body.toString();
   //message.reply(Helper.wrap(body));
-  console.log('body:', body);
+  console.log('body:', bodystr);
   });
 }
