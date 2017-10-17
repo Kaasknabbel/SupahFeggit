042f1a1922cfaffca30fc43a258579acf3f6afe5
Kaasknabbel <32313522+Kaasknabbel@users.noreply.github.com>
2017-10-17 12:37:12 +0200

    Update prison.js

--- components/prison.js
+++ components/prison.js
@@ -7,9 +7,22 @@ module.exports = Prison = function() {
 }
 
 Prison.prototype.moveToPrison(args, message) {
-  
+  if (!args[0].mentions.users.first()) 
+    return message.reply(Helper.wrap('Please mention a user to move to the prison, sir.'));
+  if (args[1]) {
+    if (!isNormalInteger(args[1]))
+      return message.reply(Helper.wrap('Please mention a valid amount of time to kick ' + args[0].mentions.users.first() + ', sir.'));
+  }
+  else {
+    return message.reply(Helper.wrap(args[0].mentions.users.first() + ' has been moved to the prison for unlimited time.));
+  }
 }
 
 Prison.prototype.releaseFromPrison(args, message) {
   
+}
+
+function isNormalInteger(str) {
+  var n = Math.floor(Number(str));
+  return String(n) === str && n > 0;
 }
