89e732f60947a619eaafbd249443804e7f1a6297
Kaasknabbel <32313522+Kaasknabbel@users.noreply.github.com>
2017-10-17 17:16:45 +0200

    Update prison.js

--- components/prison.js
+++ components/prison.js
@@ -12,8 +12,9 @@ Prison.prototype.moveToPrison = function(args, message) {
   if (message.mentions.users.size === 0) 
     return message.reply(Helper.wrap('Please mention a user to move to the prison, sir.'));
   var prisonMember = message.guild.member(message.mentions.users.first());
+  console.log(prisonMember);
   if (Helper.admins.includes(prisonMember.id))
-    return message.reply(Helper.wrap("You can't imprison an admin, feggit."));
+    return message.reply(Helper.wrap('You cannot imprison an admin, feggit.'));
   var prisonRole = message.guild.roles.find("name", "Prison");
   if (prisonMember.roles.has(prisonRole.id))
     return message.reply(Helper.wrap(prisonMember + ' is already in prison, sir.'));
