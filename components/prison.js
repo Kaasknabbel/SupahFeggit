ca43c1bf8a2f70060ccbc3b0049db6330c07af8f
Kaasknabbel <32313522+Kaasknabbel@users.noreply.github.com>
2017-10-17 12:27:00 +0200

    Create prison.js

---
+++ components/prison.js
@@ -0,0 +1,15 @@
+var Helper = require('./helper.js');
+
+var exports = {};
+
+module.exports = Prison = function() {
+  var vm = this;
+}
+
+Prison.prototype.moveToPrison(args, message) {
+  
+}
+
+Prison.prototype.releaseFromPrison(args, message) {
+  
+}
