b38d87aef67ab663d994dbc711cc489b752f33dc
Kaasknabbel <32313522+Kaasknabbel@users.noreply.github.com>
2017-10-17 14:17:53 +0200

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -302,14 +302,17 @@ function toggleSound (args, message) {
 
 function prison(args, message) {
   if (admins.includes(message.member.user.id)) {
-    Prison.moveToPrison(args, message, Bot);
+    if (Bot.memberHasRole(jh, 'Prison')) 
+      return message.reply(Helper.wrap('Yes'));
+    else return message.reply(Helper.wrap('No'));
+    //Prison.moveToPrison(args, message, Bot);
   }
   else message.reply(Helper.wrap('You need to be an admin to use this command, feggit.'));
 }
 
 function release(args, message) {
   if (admins.includes(message.member.user.id)) {
-    Prison.releaseFromPrison(args, message, Bot);
+    //Prison.releaseFromPrison(args, message, Bot);
   }
   else message.reply(Helper.wrap('You need to be an admin to use this command, feggit.'));
 }
