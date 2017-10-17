dd554ec95d130d96f386fe72adfb745dc6770a5c
Kaasknabbel <32313522+Kaasknabbel@users.noreply.github.com>
2017-10-17 15:48:56 +0200

    Update prison.js

--- components/prison.js
+++ components/prison.js
@@ -13,6 +13,7 @@ Prison.prototype.moveToPrison = function(args, message) {
     return message.reply(Helper.wrap('Please mention a user to move to the prison, sir.'));
   var prisonMember = message.guild.member(message.mentions.users.first());
   var prisonRole = message.guild.roles.find("name", "Prison");
+  var prisonerChannel = vm.checkVoicePrisoner(message, prisonMember);
   var amountOfTime = argsArray.slice(1).join(" ");
   if (amountOfTime) {
     if (!isNormalInteger(amountOfTime))
@@ -20,6 +21,9 @@ Prison.prototype.moveToPrison = function(args, message) {
     setTimeout(() => {
       vm.releaseFromPrison(args, message);
     }, amountOfTime * 1000);
+    if (prisonerChannel) {
+      console.log(prisonerChannel);
+    }
     prisonMember.addRole(prisonRole).catch(console.error);
     return message.reply(Helper.wrap(prisonMember + ' has been moved to the prison for ' + amountOfTime + ' seconds, sir.'));
   }
@@ -40,6 +44,14 @@ Prison.prototype.releaseFromPrison = function(args, message) {
     return message.reply(Helper.wrap(prisonMember + ' has been released from the prison, sir.'));
   }
   else return message.reply(Helper.wrap(prisonMember + ' is currently not imprisoned, sir.'));
+}
+
+function checkVoicePrisoner(message, prisonMember) {
+  var voiceChannelArray = message.guild.channels.filter((v) => v.type == 'voice').filter((v) => v.members.exists('id', prisonMember)).array();
+  if(voiceChannelArray.length <= 0) {
+    return undefined;
+  }
+  return voiceChannelArray[0];
 }
 
 function isNormalInteger(str) {
