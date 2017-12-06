5395a0135df0900c2b6544021a7fa55202e3d83e
Kaasknabbel <32313522+Kaasknabbel@users.noreply.github.com>
2017-12-06 19:49:28 +0100

    Update github.js

--- components/github.js
+++ components/github.js
@@ -7,9 +7,9 @@ var exports = {};
 module.exports = Github = function() {
   var vm = this;
   var client = gh.client(vm.apikey);
-  vm.ghme = client.me();
-  vm.ghuser = client.user('Kaasknabbel');
-  vm.ghrepo = client.repo('Kaasknabbel/SupahFeggit');
+  exports.ghme = client.me();
+  exports.ghuser = client.user('Kaasknabbel');
+  exports.ghrepo = client.repo('Kaasknabbel/SupahFeggit');
   Helper.keys('apikeys', ['github']).then(function(keys) {
     vm.apikey = keys.github;
   }).catch(err => {
@@ -21,8 +21,7 @@ module.exports = Github = function() {
 Github.prototype.updateVariable = function(name, content) {
   var vm = this;
   var sha = '30d74d258442c7c65512eafab474568dd706c430';
-  vm.ghme;
-  vm.ghrepo.updateContents('variables.js', 'Bot - Updated ' + name, content, sha, err => {
+  exports.ghrepo.updateContents('variables.js', 'Bot - Updated ' + name, content, sha, err => {
     console.log(err);
   });
 }
