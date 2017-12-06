5c39a7c6cc4d4d3064d37f1d4e1008b79ad2b859
Kaasknabbel <32313522+Kaasknabbel@users.noreply.github.com>
2017-12-07 00:54:04 +0100

    Update queue.js

--- components/queue.js
+++ components/queue.js
@@ -21,7 +21,7 @@ module.exports = Queue = function() {
   });
 }
 
-Queue.prototype.init = function() {
+Queue.prototype.initialise = function() {
   var vm = this;
   vm.blacklist = Github.blacklist;
   vm.blacklisturl = Github.blacklisturl;
