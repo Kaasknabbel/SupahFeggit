d159c2dbf5c60f644784b2e0f2bf4d8ee46c29f2
Kaasknabbel <32313522+Kaasknabbel@users.noreply.github.com>
2017-12-07 02:04:05 +0100

    Update github.js

--- components/github.js
+++ components/github.js
@@ -2,7 +2,6 @@ var request = require('request');
 var Helper = require('./helper.js');
 var gh = require('octonode');
 var Queue = require('./queue.js');
-Queue = new Queue();
 
 var exports = {};
 
