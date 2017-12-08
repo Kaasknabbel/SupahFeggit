148b0c561ab0b147ceaa9e551f72311a349a6349
Kaasknabbel <32313522+Kaasknabbel@users.noreply.github.com>
2017-12-08 12:01:57 +0100

    Delete variables.js

--- variables.js
+++
@@ -1,9 +0,0 @@
-var exports = {};
-
-exports.initialiseVariables = function() {
-  var vm = this;
-  vm.blacklist = ['Bizzey - Traag ft. Jozo & Kraantje Pappie (prod. Ramiks & Bizzey)'];
-  vm.blacklisturl = ['https://www.youtube.com/watch?v=1z8NpmCqvZE'];
-}
-
-module.exports = exports;
