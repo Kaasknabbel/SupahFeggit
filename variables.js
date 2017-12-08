63baa6f9160a5085de3d109ced6a4222bbee701c
Kaasknabbel <32313522+Kaasknabbel@users.noreply.github.com>
2017-12-08 11:51:22 +0100

    Bot - Updated blacklist

--- variables.js
+++ variables.js
@@ -2,11 +2,12 @@ var exports = {};
 
 exports.initialiseVariables = function() {
   var vm = this;
-  vm.blacklist = [''];
-  vm.blacklisturl = [''];
+  vm.blacklist = ['Bizzey - Traag ft. Jozo & Kraantje Pappie (prod. Ramiks & Bizzey)'];
+  vm.blacklisturl = ['https://www.youtube.com/watch?v=1z8NpmCqvZE'];
 }
 
 module.exports = exports;
+
 
 
 
