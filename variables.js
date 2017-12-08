1f853a2219da678e85b054634eaa61662ec00512
Kaasknabbel <32313522+Kaasknabbel@users.noreply.github.com>
2017-12-08 11:15:19 +0100

    Update variables.js

--- variables.js
+++ variables.js
@@ -1,2 +1,9 @@
-Blacklist: 'Bizzey - Traag ft. Jozo & Kraantje Pappie (prod. Ramiks & Bizzey)'
-Blacklisturl: 'https://www.youtube.com/watch?v=1z8NpmCqvZE'
+var exports = {};
+
+exports.initialiseVariables = function() {
+  var vm = this;
+  vm.blacklist = ['Bizzey - Traag ft. Jozo & Kraantje Pappie (prod. Ramiks & Bizzey)'];
+  vm.blacklisturl = ['https://www.youtube.com/watch?v=1z8NpmCqvZE'];
+}
+
+module.exports = exports;
