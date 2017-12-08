2922c3a90a50b8e345c87d5d8def1016f97bfa6a
Kaasknabbel <32313522+Kaasknabbel@users.noreply.github.com>
2017-12-08 11:51:17 +0100

    Bot - Updated blacklist

--- variables.js
+++ variables.js
@@ -2,11 +2,12 @@ var exports = {};
 
 exports.initialiseVariables = function() {
   var vm = this;
-  vm.blacklist = ['Nyan Cat 10 hours (original)'];
-  vm.blacklisturl = ['https://www.youtube.com/watch?v=wZZ7oFKsKzY'];
+  vm.blacklist = [''];
+  vm.blacklisturl = [''];
 }
 
 module.exports = exports;
+
 
 
 
