b133b37f1815c83a6b0a40b6f747d273b804bfa3
Kaasknabbel <32313522+Kaasknabbel@users.noreply.github.com>
2017-12-09 16:26:34 +0100

    Update playlist.js

--- components/playlist.js
+++ components/playlist.js
@@ -12,6 +12,7 @@ Playlist.prototype.newPlaylist = function(name, message) {
   var user = message.author.username;
   Github.readPlaylist(user, name, (userPlaylists,playlist,playlisturl) => {
     if (playlist[0] == undefined) {
+      Github.updatePlaylist(user, name, "");
       message.reply(Helper.wrap("Playlist '" + name + "' has been created, " + user + "."));
     }
     else message.reply(Helper.wrap("You already have a playlist with the name: '" + name + "'\nPlease give a unique name to your new playlist, feggit."));
