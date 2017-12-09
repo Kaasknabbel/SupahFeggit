258618424a3650debf3fdaedef8405ba5ad7c131
Kaasknabbel <32313522+Kaasknabbel@users.noreply.github.com>
2017-12-09 12:38:19 +0100

    Update playlist.js

--- components/playlist.js
+++ components/playlist.js
@@ -8,5 +8,12 @@ module.exports = Playlist = function() {
 }
 
 Playlist.prototype.newPlaylist = function(message, name) {
-  
+  var vm = this;
+  var user = message.author.username;
+  Github.readPlaylist(user, name, (userPlaylists,playlist,playlisturl) => {
+    if (playlist == []) {
+      message.reply(Helper.wrap("Playlist '" + name + "' has been created, " + user + "."));
+    }
+    else message.reply(Helper.wrap("You already have a playlist with the name: '" + name + "'\nPlease give a unique name to your new playlist, feggit."));
+  }
 }
