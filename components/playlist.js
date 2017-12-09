bb7ecfb311d8cec9eb4362682985305e381a2507
Kaasknabbel <32313522+Kaasknabbel@users.noreply.github.com>
2017-12-09 18:46:23 +0100

    Update playlist.js

--- components/playlist.js
+++ components/playlist.js
@@ -64,7 +64,6 @@ Playlist.prototype.showPlaylist = function(args, message) {
   }
   else {
     user = message.guild.member(message.mentions.users.first()).user.username;
-    console.log(user);
     var argsArray = args.split(" ");
     if (argsArray[1] != undefined) {
       var name = argsArray.slice(1).join(" ");
