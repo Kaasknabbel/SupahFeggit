16180d098c73436202c7ecfc315a0e34026ba807
Kaasknabbel <32313522+Kaasknabbel@users.noreply.github.com>
2017-12-09 16:39:06 +0100

    Update playlist.js

--- components/playlist.js
+++ components/playlist.js
@@ -13,8 +13,20 @@ Playlist.prototype.newPlaylist = function(name, message) {
   Github.readPlaylist(user, name, (userPlaylists,playlist,playlisturl) => {
     if (playlist[0] == undefined) {
       Github.updatePlaylist(user, name, "");
-      message.reply(Helper.wrap("Playlist '" + name + "' has been created, " + user + "."));
+      message.reply(Helper.wrap("Playlist '" + name + "' has been created, feggit."));
     }
     else message.reply(Helper.wrap("You already have a playlist with the name: '" + name + "'\nPlease give a unique name to your new playlist, feggit."));
+  });
+}
+
+Playlist.prototype.deletePlaylist = function(name, message) {
+  var vm = this;
+  var user = message.author.username;
+  Github.readPlaylist(user, name, (userPlaylists,playlist,playlisturl) => {
+    if (playlist[0] != undefined) {
+      Github.updatePlaylist(user, name, "-1");
+      message.reply(Helper.wrap("Playlist '" + name + "' has been deleted, feggit."));
+    }
+    else message.reply(Helper.wrap("You don't have a playlist with the name: '" + name + "', feggit."));
   });
 }
