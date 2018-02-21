f75960641e6c3af60a98fcddc820f193bb793936
Kaasknabbel <32313522+Kaasknabbel@users.noreply.github.com>
2018-02-21 22:32:58 +0100

    Update musicV2.js

--- components/musicV2.js
+++ components/musicV2.js
@@ -5,16 +5,16 @@ var exports = {};
 
 module.exports = Music = function() {
   var vm = this;
-
-  const music = new Music(client, {
-    prefix: ".", // Prefix for the commands.
-    global: true,            // Non-server-specific queues.
-    maxQueueSize: 50,        // Maximum queue size of 25.
-    clearInvoker: true,      // If permissions applicable, allow the bot to delete the messages that invoke it.
-    helpCmd: 'mhelp',        // Sets the name for the help command.
-    playCmd: 'music',        // Sets the name for the 'play' command.
-    volumeCmd: 'adjust',     // Sets the name for the 'volume' command.
-    leaveCmd: 'begone',      // Sets the name for the 'leave' command.
-    disableLoop: true        // Disable the loop command.
-  });
 }
+
+const music = new Music(client, {
+  prefix: ".", // Prefix for the commands.
+  global: true,            // Non-server-specific queues.
+  maxQueueSize: 50,        // Maximum queue size of 25.
+  clearInvoker: true,      // If permissions applicable, allow the bot to delete the messages that invoke it.
+  helpCmd: 'mhelp',        // Sets the name for the help command.
+  playCmd: 'music',        // Sets the name for the 'play' command.
+  volumeCmd: 'adjust',     // Sets the name for the 'volume' command.
+  leaveCmd: 'begone',      // Sets the name for the 'leave' command.
+  disableLoop: true        // Disable the loop command.
+});
